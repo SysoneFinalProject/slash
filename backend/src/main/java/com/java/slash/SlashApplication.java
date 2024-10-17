14448b9709276ab4a62f48eba0e49e9430693aa5
JUNI <junehee107@naver.com>
2024-10-17 14:46:34 +0900

    🎉 Build : Springboot + JPA 백엔드 초기 구성 설정

---
+++ backend/src/main/java/com/java/slash/SlashApplication.java
@@ -0,0 +1,13 @@
+package com.java.slash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlashApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/java/slash/SlashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.slash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
